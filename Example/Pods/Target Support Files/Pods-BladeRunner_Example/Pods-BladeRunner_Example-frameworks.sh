327288461f32713c8fca2eaf1c89cc1f255cd9e9
liam <liamxujia@hotmail.com>
2020-08-03 13:47:21 +0800

    [lib] add Gifu SwiftyGif

--- Example/Pods/Target Support Files/Pods-BladeRunner_Example/Pods-BladeRunner_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-BladeRunner_Example/Pods-BladeRunner_Example-frameworks.sh
@@ -198,9 +198,13 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BladeRunner/BladeRunner.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Gifu/Gifu.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyGif/SwiftyGif.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BladeRunner/BladeRunner.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Gifu/Gifu.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyGif/SwiftyGif.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
